0181f2b14e62f3cf5ce34dd62f5749b32010b9a6
Kiran <kirandeepkaurfdk5@gmail.com>
2023-05-11 18:08:04 -0800

    list and table added to the hero section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -12,16 +12,39 @@ function home() {
       </div>
       <div className="h-three">
         Home Content Section
+        <h1>Paragraph</h1>
         <p className="h-para">
           A basic webpage typically consists of a header with the website name
           and navigation menu, a content area that displays the main content, a
           sidebar for additional information or links, and a footer with
-          important information. Links and images are also important elements
-          that add functionality and visual appeal to a webpage. By combining
-          these elements in a cohesive and user-friendly design, you can create
-          a basic webpage that effectively communicates your message to your
-          audience.
+          important information.
         </p>
+        <h2>List</h2>
+        <ul className="h-text">
+          <li>Nav bar</li>
+          <li>Slider</li>
+          <li>Header</li>
+          <li>Footer</li>
+          <li>Content Section</li>
+        </ul>
+        <h2>Add a table:</h2>
+        <table className="h-table">
+          <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Savings</th>
+          </tr>
+          <tr>
+            <td>Kiran</td>
+            <td>Brar</td>
+            <th>$100</th>
+          </tr>
+          <tr>
+            <td>Bhupinder</td>
+            <td>Dhillon</td>
+            <th>$150</th>
+          </tr>
+        </table>
       </div>
     </>
   );
--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -11,16 +11,39 @@ function Product() {
       </div>
       <div className="p-three">
         Product Content Section
+        <h1>Paragraph</h1>
         <p className="p-para">
           A basic webpage typically consists of a header with the website name
           and navigation menu, a content area that displays the main content, a
           sidebar for additional information or links, and a footer with
-          important information. Links and images are also important elements
-          that add functionality and visual appeal to a webpage. By combining
-          these elements in a cohesive and user-friendly design, you can create
-          a basic webpage that effectively communicates your message to your
-          audience.
+          important information.
         </p>
+        <h2>List</h2>
+        <ul className="p-text">
+          <li>Nav bar</li>
+          <li>Slider</li>
+          <li>Header</li>
+          <li>Footer</li>
+          <li>Content Section</li>
+        </ul>
+        <h2>Add a table:</h2>
+        <table className="p-table">
+          <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Savings</th>
+          </tr>
+          <tr>
+            <td>Kiran</td>
+            <td>Brar</td>
+            <th>$100</th>
+          </tr>
+          <tr>
+            <td>Bhupinder</td>
+            <td>Dhillon</td>
+            <th>$150</th>
+          </tr>
+        </table>
       </div>
     </>
   );
